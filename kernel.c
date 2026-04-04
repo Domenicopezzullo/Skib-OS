f1b61c43b0383d96d7a0e146c9760344712c9f64
Domenicopezzullo <pezzullodomenico712@gmail.com>
2026-04-04 23:07:42 +0200

    clangd: ignore keyboard.h unused include

--- kernel.c
+++ kernel.c
@@ -12,13 +12,12 @@ extern void mouse_handler_wrapper(void);
 extern void timer_handler_wrapper(void);
 
 #include "drivers/idt.h"
-#include "drivers/keyboard.h"
+// needed for keyboard_handler
+#include "drivers/keyboard.h" // IWYU pragma: keep
 #include "drivers/mouse.h"
 #include "drivers/ui.h"
 
-__attribute__((section(".text.entry"))) void _start() {
-  clear_screen(0x001C6BA8);
-
+void init_state() {
   pic_remap();
   set_idt_gate(32, (unsigned int)timer_handler_wrapper);
   set_idt_gate(33, (unsigned int)keyboard_handler_wrapper);
@@ -26,7 +25,11 @@ __attribute__((section(".text.entry"))) void _start() {
   load_idt();
 
   mouse_install();
+}
 
+__attribute__((section(".text.entry"))) void _start() {
+  clear_screen(0x001C6BA8);
+  init_state();
   draw_rect(0, SCREEN_HEIGHT - 40, SCREEN_WIDTH, 40, 0x00D4D0C8);
   draw_hline(0, SCREEN_HEIGHT - 40, SCREEN_WIDTH, 0x00FFFFFF);
   __asm__ volatile("sti");
